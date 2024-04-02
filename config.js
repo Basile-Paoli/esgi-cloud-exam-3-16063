33ba3f06ad35fcd3258fbad0bd6958726dc49904
Basile Paoli <basile.paoli@gmail.com>
2024-04-02 12:27:52 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'JTNVP49BAKAHCA0AUI7X', // TODO
+    secretAccessKey: '5ThVD6lDqdNMXtAuYdOEe4SCrHi2cdMeA8kLiUUl', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-16063', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
